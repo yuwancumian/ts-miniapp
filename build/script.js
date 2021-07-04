5d7f0843b81aa4a4207690082ea3fc76e10106ad
yuwancumian <yuwancumian@hotmail.com>
2021-07-04 22:57:06 +0800

    增加ts编译错误处理

--- build/script.js
+++ build/script.js
@@ -1,10 +1,30 @@
 const gulp = require("gulp");
 const { typescript, sourcemaps } = require("gulp-load-plugins")();
-const tsProject = typescript.createProject("tsconfig.json");
+const plumber = require("gulp-plumber");
 const cwd = process.cwd();
 const path = require("path");
+function swallowError(error) {
+  // If you want details of the error in the console
+  console.error(error.toString());
+
+  this.emit("end");
+}
+
+const tsProject = typescript.createProject("tsconfig.json", { typescript: require("typescript"), noResolve: true });
 module.exports = function compileJs(filePath) {
   let file = "src/**/*.ts";
   let dist = "dist";
-  return tsProject.src().pipe(sourcemaps.init()).pipe(tsProject()).js.pipe(sourcemaps.write()).pipe(gulp.dest("dist"));
+  return gulp
+    .src(file)
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          console.log(err);
+        },
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js.pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist"));
 };
